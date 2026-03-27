1184de6181b9877a51e31d83f2b3600bd5574481
nitu <124525218+Shamsun-Nahar-Nitu@users.noreply.github.com>
2026-03-27 21:58:40 +0600

    Fix formatting in URL configuration file by adding a newline at the end

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -27,3 +27,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
