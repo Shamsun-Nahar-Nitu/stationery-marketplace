6e1c3564fe52f259a3e06e28533a765c9381da68
nitu <124525218+Shamsun-Nahar-Nitu@users.noreply.github.com>
2026-03-31 21:47:57 +0600

    Add subtotal and total fields to Order model

--- orders/models.py
+++ orders/models.py
@@ -16,6 +16,9 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_PENDING)
 
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default="0.00")
+    total = models.DecimalField(max_digits=10, decimal_places=2, default="0.00")
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
